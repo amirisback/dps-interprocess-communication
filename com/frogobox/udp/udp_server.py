87f29df7257fb4d66db3c31601377f3b8b58c13b
amirisback <faisalamircs@gmail.com>
2020-02-12 12:18:13 +0700

    add: library socket

--- com/frogobox/udp/udp_server.py
+++ com/frogobox/udp/udp_server.py
@@ -1,23 +1,7 @@
-#
-# Created by Faisal Amir
-# FrogoBox Inc License
-# -----------------------------------------
-# Interprocess-Communication
-# Copyright (C) 11/02/2020.      
-# All rights reserved
-# -----------------------------------------
-# Name     : Muhammad Faisal Amir
-# E-mail   : faisalamircs@gmail.com
-# Github   : github.com/amirisback
-# LinkedIn : linkedin.com/in/faisalamircs
-# -----------------------------------------
-# FrogoBox Software Industries
-# 
-#/
-
 # /** START CODE **/
 
 # import library socket karena akan menggunakan IPC socket
+import socket
 
 # Berikan tampilan (print bahwa socket berhasil dibuat)
 
